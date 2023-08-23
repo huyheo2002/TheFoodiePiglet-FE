4efa287ab7637df0682ffe75b677804c56c42f17
BuiCao <buivancao2002ba@gmail.com>
2023-08-23 16:33:55 +0700

    Committing changes before merge

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,13 +7,14 @@ import Login from "../pages/Login";
 import Test from "../pages/Test";
 import DashBoard from "../pages/Admin/Dashboard";
 import UserManagement from "../pages/Admin/UserManagement";
+import News from "../pages/News";
 
 // k đăng nhập sd đc
 const publicRoutes = [
     { path: "/", component: Home, layout: DefaultLayout },
     { path: "/login", component: Login, layout: null },
     { path: "/test", component: Test, layout: null },
-    
+    { path: "/news", component: News, layout: DefaultLayout },
     // system
     { path: "/system", component: DashBoard, layout: AdminLayout },
     { path: "/system/users", component: UserManagement, layout: AdminLayout },
