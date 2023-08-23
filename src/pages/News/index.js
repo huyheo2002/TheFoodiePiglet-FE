e3e7672a50dedd7efd0d8ff23f5f9dc7c369b83a
BuiCao <buivancao2002ba@gmail.com>
2023-08-23 21:02:20 +0700

    Test page News

---
+++ src/pages/News/index.js
@@ -0,0 +1,11 @@
+import Menu from "./menu"; 
+function News() {
+    return (  
+        <div className="text-white">
+           
+        <Menu/>
+        </div>
+    );
+}
+
+export default News;
